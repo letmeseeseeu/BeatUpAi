c03afba0da695b645982218afdf4cfdab700ff26
letmeseeseeu <err.objectnotfound@gmail.com>
2025-08-30 22:15:30 +0800

    Update DD.h

--- DD.h
+++ DD.h
@@ -1,16 +1,16 @@
 #pragma once
 #include <Windows.h>
 
-// 与原 DLL 完全一致的函数签名（__stdcall / WINAPI）
+
 typedef int (WINAPI* pDD_btn)(int btn);
 typedef int (WINAPI* pDD_whl)(int whl);
 typedef int (WINAPI* pDD_key)(int keycode, int flag);
 typedef int (WINAPI* pDD_mov)(int x, int y);
-typedef int (WINAPI* pDD_str)(const char* str);   // DLL 是窄字符
+typedef int (WINAPI* pDD_str)(const char* str);   // DLL 鏄獎瀛楃
 typedef int (WINAPI* pDD_todc)(int vk);
 typedef int (WINAPI* pDD_movR)(int dx, int dy);
 
-// 纯 Win32 版本：不使用 CString / StdAfx
+
 class CDD
 {
 public:
@@ -23,7 +23,7 @@ public:
         if (m_hModule) { ::FreeLibrary(m_hModule); m_hModule = nullptr; }
     }
 
-    // 兼容 MFC & 非 MFC：LPCWSTR 可由 CString 隐式转换
+
     int GetFunAddr(LPCWSTR dllfile);
 
 public:
@@ -38,3 +38,4 @@ public:
 private:
     HMODULE m_hModule;
 };
+
